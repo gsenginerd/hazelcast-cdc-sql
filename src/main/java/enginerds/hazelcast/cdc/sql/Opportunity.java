fe2e673ec7541b8962f9b63cb81e7b88d224a895
Subramaniam G <sgopalakrishnan@hl.com>
2022-11-16 19:32:40 +0400

    Base code

---
+++ src/main/java/enginerds/hazelcast/cdc/sql/Opportunity.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package enginerds.hazelcast.cdc.sql;
+
+/**
+ * @author SGopala
+ *
+ */
+public class Opportunity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
