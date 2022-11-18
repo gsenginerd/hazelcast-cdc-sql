ebca219a574f6ed147c63e5607a5d39487b61dd1
Subramaniam G <sgopalakrishnan@hl.com>
2022-11-18 12:26:49 +0400

    Added initial code for connector class and builder

--- src/main/java/enginerds/hazelcast/cdc/sql/Opportunity.java
+++ src/main/java/enginerds/hazelcast/cdc/sql/Opportunity.java
@@ -3,6 +3,16 @@
  */
 package enginerds.hazelcast.cdc.sql;
 
+import com.fasterxml.jackson.databind.ext.SqlBlobSerializer;
+import com.hazelcast.jet.cdc.ChangeRecord;
+import com.hazelcast.jet.cdc.DebeziumCdcSources;
+import com.hazelcast.jet.cdc.DebeziumCdcSources.Builder;
+import com.hazelcast.jet.pipeline.StreamSource;
+import com.hazelcast.sql.SqlService;
+import com.hazelcast.sql.impl.schema.SqlCatalog;
+
+import io.debezium.connector.sqlserver.SqlServerConnector;
+
 /**
  * @author SGopala
  *
@@ -13,7 +23,18 @@ public class Opportunity {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SqlServerConnector sqlConnector = new SqlServerConnector();
+		Builder<ChangeRecord> builder= DebeziumCdcSources.debezium("sf_opportunity", sqlConnector.getClass());
+		builder.setProperty("database.hostname", "slv-haldevdb");
+		builder.setProperty("database.port", "1433");
+		builder.setProperty("database.user", "conflict_check_admin");
+		builder.setProperty("database.password", "SForcedev1");
+		builder.setProperty("database.dbname", "CONFLICT_CHECK");
+		
+		StreamSource<ChangeRecord> source = builder.build();
+		
+	
 
 	}
 
